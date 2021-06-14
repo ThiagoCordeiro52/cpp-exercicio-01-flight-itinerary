552fd250c68bc9ac78d0086909329513a318c70a
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2021-06-14 10:33:58 -0300

    Versao completa sem documentacao  (12/12)

--- source/src/route.cpp
+++ source/src/route.cpp
@@ -13,36 +13,22 @@ std::string find_route( const std::string & start, Pair segs[], size_t sz )
 {
     // TODO: Add your solution here.
     int doBreak = 0;
+    int valueOne = -1;
+    int valueTwo = -1;
     int used[sz];
     int pos = 0;
+    int positions[sz];
     int positives = 0;
     for (int i = 0; i < sz; i++) 
     {
         used[i] = -1;
+        positions[i] = -1;
     }
 
     std::string value = start;
 
-    std::cout << "Start: " << start <<std::endl;
-
-    std::cout << "rotas: " << std::endl;
-
-    for(int i = 0; i < sz; i++) {
-        std::cout << "origem: " << segs[i].orig << " destino: " << segs[i].dest << std::endl;
-    }
-
-    
-
     // Empty route.
     std::ostringstream route{""}; // Stores the airports that compose the route.
-
-    if(sz == 1) 
-    {
-        route << segs[0].orig;
-        route << " " << segs[0].dest;
-        std::cout << route.str() << std::endl;
-        return route.str() ;
-    }
 
     for (int j = 0; j < sz; j++) 
     {
@@ -59,15 +45,31 @@ std::string find_route( const std::string & start, Pair segs[], size_t sz )
 
             if(segs[i].orig == value && doBreak == 0) 
             {
+                for(int w = i + 1; w < sz; w++) {
+
+                    doBreak = 0;
+                    for (int z = 0; z < sz; z++) 
+                    {
+                        if(w == used[z]) {
+                            doBreak = 1;
+                            break;
+                        }
+                    }
+
+                    if(segs[w].orig == value && doBreak == 0 && segs[w].dest < segs[i].dest) {
+                        i = w;
+                    } 
+                }
+
                 route << value << " ";
                 value = segs[i].dest;
                 used[pos] = i;
                 pos++;
                 break;
             }
-        }    
-    }
+        }
 
+    }
     route << value;
 
     for (int i = 0; i < sz; i++) {
@@ -75,12 +77,10 @@ std::string find_route( const std::string & start, Pair segs[], size_t sz )
             positives += 1;
         }
     }
-    if(positives < sz) {
+
+    if(positives < sz || sz == 0) {
         return "null";
     }
-
-    std::cout << route.str() << std::endl;
-
 
     return route.str() ; // Stub: replace this return as you see fit.
 }
