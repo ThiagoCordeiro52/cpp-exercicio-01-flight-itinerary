5c3f2bf6e3f29eb98fdf8b60cd2220d238fbad82
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2021-06-14 09:05:21 -0300

    Versao com analise de repeticao e nulos  (7/12)

--- source/src/route.cpp
+++ source/src/route.cpp
@@ -12,6 +12,14 @@
 std::string find_route( const std::string & start, Pair segs[], size_t sz )
 {
     // TODO: Add your solution here.
+    int doBreak = 0;
+    int used[sz];
+    int pos = 0;
+    int positives = 0;
+    for (int i = 0; i < sz; i++) 
+    {
+        used[i] = -1;
+    }
 
     std::string value = start;
 
@@ -40,10 +48,21 @@ std::string find_route( const std::string & start, Pair segs[], size_t sz )
     {
         for(int i = 0; i < sz; i++)
         {
-            if(segs[i].orig == value) 
+            doBreak = 0;
+            for (int z = 0; z < sz; z++) 
+            {
+                if(i == used[z]) {
+                    doBreak = 1;
+                    break;
+                }
+            }
+
+            if(segs[i].orig == value && doBreak == 0) 
             {
                 route << value << " ";
                 value = segs[i].dest;
+                used[pos] = i;
+                pos++;
                 break;
             }
         }    
@@ -51,7 +70,17 @@ std::string find_route( const std::string & start, Pair segs[], size_t sz )
 
     route << value;
 
+    for (int i = 0; i < sz; i++) {
+        if(used[i] >= 0) {
+            positives += 1;
+        }
+    }
+    if(positives < sz) {
+        return "null";
+    }
+
     std::cout << route.str() << std::endl;
+
 
     return route.str() ; // Stub: replace this return as you see fit.
 }
